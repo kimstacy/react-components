e9d46f8c1a58e49b297b2b625261a2d32edc3b50
Stacy Kim <stacy.kim@ucla.edu>
2019-11-05 14:42:56 -0600

    Creating and rendering nested React components

--- app.js
+++ app.js
@@ -1,25 +1,55 @@
 /*
-Creating and Rendering a React component
+1. Creating and Rendering a React component
 
 Inside app.js, create a GroceryList component that contains an unordered list of 2 grocery items. Render this component to the div tag in index.html with an id of app
 */
 
 // Create a React component
+// var GroceryList = () => (
+//   <div>
+//     <h2>My Grocery List</h2>
+//     <ul>
+//       <li>Apples</li>
+//       <li>Tea</li>
+//     </ul>
+//   </div>
+// );
+
+/* Render a React component - Two Arguments: Argument 1 is the componenent we want to render in <>, Argument 2 is the location we want to render it, using the id.
+
+ReactDOM.render(component, where we want component to render);
+ReactDOM.render(componentInstance, DOMElement);
+ReactDOM.render(<App />, document.getElementById("actual-dom-element-where-I-want-to-render-my-component"));
+*/
+
+// ReactDOM.render(<GroceryList />, document.getElementById("app"));
+
+
+/*
+2. Creating and Rendering nested React components
+
+Create React components for the 2 items in your grocery list. For example, if your grocery list contains "cucumbers" and "kale", create a Cucumbers component and a Kale component
+
+Use these two new components inside your GroceryList component instead of the hardcoded <li>s
+*/
+
 var GroceryList = () => (
   <div>
     <h2>My Grocery List</h2>
     <ul>
-      <li>Apples</li>
-      <li>Tea</li>
+      <Apples />
+      <Tea />
     </ul>
   </div>
 );
 
-// Render a React component - Two Arguments: Argument 1 is the componenent we want to render in <>, Argument 2 is the location we want to render it, using the id.
+// Create a new component for each grocery list item
+ var Apples = () => (
+   <li>Apples</li>
+ )
 
-// ReactDOM.render(component, where we want component to render);
-// ReactDOM.render(componentInstance, DOMElement);
-// ReactDOM.render(<App />, document.getElementById("actual-dom-element-where-I-want-to-render-my-component"));
+ var Tea = () => (
+   <li> Tea</li>
+ )
 
-ReactDOM.render(<GroceryList />, document.getElementById("app"));
-
+ ReactDOM.render(<GroceryList />, document.getElementById("app"));
